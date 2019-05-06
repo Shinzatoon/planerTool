b0f9cae989b8facb160e3ef54dfde8ca27590bb6
gecko <gecko@LAPTOP-74FA5EHG>
2019-05-06 13:27:37 +0900

    Revert "ちょこちょこ"
    This reverts commit dbda38efbd8e04a5dd674e1e48b54f63c08dd631.

--- Title.cpp
+++ Title.cpp
@@ -6,7 +6,7 @@
 #include "CircDblLinkedList.h"
 
 Image back;
-Image back1[5];
+Image back1;
 Image back2;
 Image back3;	//後で配列にしよう！！
 Dlist objList;
@@ -25,56 +25,65 @@ bool onDrag2 = false;//ドラッグされているか
 bool oncursor(Image);
 
 void initializeTitle() {
-	int i;
-
 	InitImage(&back, getTexture(textureLoaderNS::BACK_GROUND), 0, 0,WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	for (i = 0; i < 5; i++)
-	{
-		InitImage(&back1[i], getTexture(textureLoaderNS::ENEMY_ICON), i*100, 160, 100, 100);
-	}
-
+	InitImage(&back1, getTexture(textureLoaderNS::ENEMY_ICON), 100, 160, 100, 100);
 	InitImage(&back2, getTexture(textureLoaderNS::PLAYER_ICON), 100, 320, 100, 100);
 	InitImage(&back3, getTexture(textureLoaderNS::STAR_ICON), 100, 480, 100, 100);
 	
 	Initialize(&objList);//リストの初期化
 
-	//InsertAfter(&objList, objList.crnt, new Image());//新しいImage構造体を追加する 登録　new
-	//InitImage(&objList.crnt->data, getTexture(textureLoaderNS::ENEMY_ICON), 100, 160, 100, 100);//現在さしているノードを初期化
-	//
+	InsertAfter(&objList, objList.crnt, new Image());//新しいImage構造体を追加する 登録　new
+	InitImage(&objList.crnt->data, getTexture(textureLoaderNS::ENEMY_ICON), 100, 160, 100, 100);//現在さしているノードを初期化
+	
 };
 void updateTitle() {
-	int i;
 
-	for (i = 0; i < 5; i++)
-	{
-		moveControl(&back1[i]);
-	}
+	moveControl(&back1);
 
 	moveControl(&back2);
+	//if (getMouseLButton())
+	//{//左クリックが押されているとき
+	//	if (oncursor(back1)) 
+	//	{//カーソルがアイコン上にあるとき
+	//		if (!onDrag)
+	//		{//何かをドラッグしていなければ
+	//			//そのアイコンをドラッグする
+	//			recordCursor = VECTOR2(getMouseX(), getMouseY());//クリックした瞬間のカーソル位置を保存
+	//			recordIcon = (VECTOR2)back1.position;//クリックした瞬間のアイコン位置を保存
+	//			onDrag = true;//ドラッグ状態にする
+	//		}
+	//	}
+	//	//setPosition(&back1, back1.position.x += (float)getMouseRawX(), back1.position.y += (float)getMouseRawY());
+	//	//setPosition(&back1, (float)getMouseRawX(), (float)getMouseRawY());
+	//}
+	//else if (onDrag)
+	//{//左クリックが離されていて、ドラッグ状態であったならば
+	//	onDrag = false;//ドラッグ状態をOFFにする
+	//}
+
+	//if (onDrag)
+	//{//ドラッグ状態のとき移動
+	//	cursorMoveAmount = VECTOR2(getMouseX(), getMouseY()) - recordCursor;//カーソル移動量
+	//	VECTOR2 virtualPos = recordIcon + cursorMoveAmount;//仮の表示位置
+	//	setPosition(&back1, virtualPos.x, virtualPos.y);
+	//}
+	
+
+	
+
 
 };
 void drawTitle() {
-	int i = 0;
-
 	DrawImage(&back);
-
-	
-	for (i = 0; i < 5; i++)
-	{
-		DrawImage(&back1[i]);
-	}
-
+	DrawImage(&back1);
 	DrawImage(&back2);
 	DrawImage(&back3);
 
 
-	//DrawImage(&objList.crnt->data);//現在さしているノードを描画する
+	DrawImage(&objList.crnt->data);//現在さしているノードを描画する
 
 };
 void printTitle() {
-	int i;
-
 	printTextDX(getDebugFont(), "Title", 0, 0);
 	printTextDX(getDebugFont(), "mouseX:", 1000, 0, getMouseX());
 	printTextDX(getDebugFont(), "mouseY:", 1000, 20, getMouseY());
@@ -84,7 +93,7 @@ void printTitle() {
 		printTextDX(getDebugFont(), "mouseY:", 500, 20, getMouseRawY());
 
 	}
-	if (oncursor(back1[0]))
+	if (oncursor(back1))
 	{
 		printTextDX(getDebugFont(), "挿入った", 500, 0);
 	}
